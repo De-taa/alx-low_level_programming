b5bd5708b96079fce57c8be764d50125e8507837
Pelumi Awobodu <pelumiawobodu@gmail.com>
2024-06-25 14:23:24 -0700

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,10 +4,10 @@
  * add - adds two integers and returns the result
  * @a: first integer
  * @b: second integer
- * 
+ *
  * Return: sum of a and b
  */
 int add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
