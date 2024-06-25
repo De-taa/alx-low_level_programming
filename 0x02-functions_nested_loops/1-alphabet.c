f1ec1779791609041f84af5338a8d20f2d0edf8a
Pelumi Awobodu <pelumiawobodu@gmail.com>
2024-06-25 11:27:30 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - main function of alphabeth
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
