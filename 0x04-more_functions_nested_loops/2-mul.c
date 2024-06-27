fb5d4122f3a78c6d965b3e8c38525e3560c803b1
Pelumi Awobodu <pelumiawobodu@gmail.com>
2024-06-27 14:49:16 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function that checks for a digit
+ * @a: a -  variable
+ * @b: b - variable
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
