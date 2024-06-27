27e85997caede311a7b652740a58ba218c25f9d7
Pelumi Awobodu <pelumiawobodu@gmail.com>
2024-06-27 15:23:14 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - function to check for a digit
- *
+ * print_numbers - function to print numbers from 0 to 9
  * Return: always 0
  */
 void print_numbers(void)
 {
 	int pr;
 
-	for (pr = 48; pr < 57; pr++)
+	for (pr = 48; pr <= 57; pr++)
 	{
 		_putchar(pr);
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
