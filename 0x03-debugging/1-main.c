51a61d1aeb14b45f304d9994ab1890d305b77301
Pelumi Awobodu <pelumiawobodu@gmail.com>
2024-06-27 19:40:43 -0700

    Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9, followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
