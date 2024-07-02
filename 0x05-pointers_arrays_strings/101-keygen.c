0bcfc11a8bae9c76fe98f85b3e16cf81f7961073
Pelumi Awobodu <pelumiawobodu@gmail.com>
2024-07-01 21:36:30 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,16 +10,25 @@
 
 int main(void)
 {
-	int i;
-	char password[12];
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
 
 	srand(time(NULL));
-	for (i = 0; i < 11; i++)
+
+	for (i = 0; i < 100; i++)
 	{
-		password[i] = rand() % 94 + 33;
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[1] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	password[11] = '\0';
-	printf("%s", password);
 	return (0);
 }
-
