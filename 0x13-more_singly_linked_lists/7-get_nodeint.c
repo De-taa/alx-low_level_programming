942e9ad90af05218ac9942bf506e4ec585bbc256
Pelumi Awobodu <pelumiawobodu@gmail.com>
2024-08-06 14:31:26 -0700

    Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,11 +7,13 @@
  * @index: the returning index.
  * Return: Null
  */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
 	listint_t *temporal;
+
 	while (head)
 	{
 	temporal = head;
