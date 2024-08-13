88f38aff5a2c03678d37132343fb1cf7e7bb0d2e
Pelumi Awobodu <pelumiawobodu@gmail.com>
2024-08-12 18:05:46 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
+/**
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	f(name);
-}
 
-int main(void)
-{
-	print_name("Bob", print_name_as_is);
-	print_name("Bob Dylan", print_name_uppercase);
-	printf("\n");
-	return (0);
+	f(name);
 }
